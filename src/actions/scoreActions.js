075fbb8f5d4e1846f4fc1dd001e28d8cc945beaa
oliver-norden <oliver.norden@axema.se>
2019-09-19 10:28:06 +0200

    Console logging of possible scores of multiples (Pairs, etc)

--- src/actions/scoreActions.js
+++ src/actions/scoreActions.js
@@ -11,9 +11,46 @@ export const calcPossibleScores = () => (dispatch, getState) => {
     const diceCounts = {};
     dice.forEach(dice => (diceCounts[dice]) ? diceCounts[dice]++ : diceCounts[dice] = 1);
 
-    // Fill possible scores objects with singels (ones, twos, etc)
-    for (const dice in diceCounts) {
+    // Special scores
+    const multiples = [
+        {
+            name: 'pairs',
+            criteria: 2,
+            multiplier: 2
+        },
+        {
+            name: 'threeOfAKind',
+            criteria: 3,
+            multiplier: 3
+        },
+        {
+            name: 'fourOfAKind',
+            criteria: 4,
+            multiplier: 4
+        },
+        {
+            name: 'yatzy',
+            criteria: 5,
+            score: 50
+        }
+    ];
+
+    // Fill possible scores object
+    for (let dice in diceCounts) {
+
+        // Singels (ones, twos, etc)
         possibleScores[dice] = dice * diceCounts[dice];
+
+        // Multiples
+        for (const multiple of multiples) {
+            const { name, criteria, score, multiplier } = multiple;
+
+            if (diceCounts[dice] >= criteria) {
+                if (!possibleScores[name]) possibleScores[name] = {}; // Initiate object
+                possibleScores[name][dice] = score || dice * multiplier; // Assign the possible score with a fixed score of a multiplier
+            }
+        }
+
     }
 
     dispatch({
