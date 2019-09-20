c84afa78d8b8cedec23dd57899e99e6d081d76e2
oliver-norden <oliver.norden@axema.se>
2019-09-20 12:53:14 +0200

    Combined specials and combinations (straigths etc and three of akind etc) in one object

--- src/actions/scoreActions.js
+++ src/actions/scoreActions.js
@@ -11,75 +11,72 @@ export const calcPossibleScores = () => (dispatch, getState) => {
     const diceCounts = {};
     dice.forEach(dice => (diceCounts[dice]) ? diceCounts[dice]++ : diceCounts[dice] = 1);
 
-    const specials = [
+    const combinations = [
+        {
+            name: 'one pair',
+            valueCounts: [2]
+        },
+        {
+            name: 'threeOfAKind',
+            valueCounts: [3]
+        },
+        {
+            name: 'fourOfAKind',
+            valueCounts: [4]
+        },
+        {
+            name: 'two pairs',
+            valueCounts: [2, 2]
+        },
+        {
+            name: 'full house',
+            valueCounts: [3, 2]
+        },
+        {
+            name: 'yatzy',
+            valueCounts: [5],
+            score: 50
+        },
         {
             name: 'small straight',
-            criteria: [1, 2, 3, 4, 5],
+            values: [1, 2, 3, 4, 5],
             score: 15
         },
         {
             name: 'large straight',
-            criteria: [2, 3, 4, 5, 6],
+            values: [2, 3, 4, 5, 6],
             score: 20
-        }
-    ];
-
-    const combinations = [
-        {
-            name: 'one pair',
-            criteria: [2]
-        },
-        {
-            name: 'threeOfAKind',
-            criteria: [3]
-        },
-        {
-            name: 'fourOfAKind',
-            criteria: [4]
-        },
-        {
-            name: 'two pairs',
-            criteria: [2, 2]
-        },
-        {
-            name: 'full house',
-            criteria: [3, 2]
-        },
-        {
-            name: 'yatzy',
-            criteria: [5],
-            score: 50
         }
 
     ]
 
-    // Check for specials (Straights etc)
-    for (const special of specials) {
-        const { name, criteria, score } = special;
-
-        if (criteria.every(reqVal => dice.includes(reqVal))) possibleScores[name] = score // If all criterial dice are rolled
-    }
-
     // Check for combinations (Pairs, full house etc)
     for (const combination of combinations) {
-        const { criteria, name, score } = combination;
-        criteria.sort(); // The most dice should priorotize the highest value
-        let validDice = [];
-        let diceSum = 0;
-        
-        // Loop through criteria and check for count in dice count and add to validDice if not already there
-        for (const criteriaCount of criteria) {
-            let highestValidDice;
-            for (const diceVal in diceCounts) {
-                if (diceCounts[diceVal] >= criteriaCount && !validDice.includes(Number(diceVal))) highestValidDice = Number(diceVal); // Select highest valid dice
-            }
-            if (highestValidDice) {
-                diceSum += highestValidDice * criteriaCount;
-                validDice.push(highestValidDice);
-            }
+        const { valueCounts, values, name, score } = combination;
+
+        if (values) { // If exact values are required, for example, in a straight
+            if (values.every(reqVal => dice.includes(reqVal))) possibleScores[name] = score // If all criterial dice are rolled
         }
 
-        if (validDice.length === criteria.length) possibleScores[name] = score || diceSum; // If length of valid dice and criteria array are equal, all critera are met
+        else { // If exact values are NOT required, for example, in a four of a kind
+            valueCounts.sort(); // The most dice should priorotize the highest value, i.e pick value first
+            let validDice = [];
+            let diceSum = 0;
+            
+            // Loop through criteria and check for count in dice count and add to validDice if not already there
+            for (const valueCount of valueCounts) {
+                let highestValidDice;
+                for (const diceVal in diceCounts) {
+                    if (diceCounts[diceVal] >= valueCount && !validDice.includes(Number(diceVal))) highestValidDice = Number(diceVal); // Select highest valid dice
+                }
+                if (highestValidDice) {
+                    diceSum += highestValidDice * valueCount;
+                    validDice.push(highestValidDice);
+                }
+            }
+    
+            if (validDice.length === valueCounts.length) possibleScores[name] = score || diceSum; // If length of valid dice and criteria array are equal, all critera are met
+        }
     };
 
     // Fill possible scores with chance
