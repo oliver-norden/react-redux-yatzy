3fd80117fcca33344556622d1fcbaaa5c2290100
oliver-norden <oliver.norden@axema.se>
2019-09-20 12:54:34 +0200

    Possible score reducer fixes

--- src/reducers/scoreReducers.js
+++ src/reducers/scoreReducers.js
@@ -1,21 +1,16 @@
 import { CALC_POSSIBLE_SCORES } from '../actions/types';
 
 const initialState = {
-    possibleScores: {
-        ones: 0,
-        twos: 0,
-        threes: 0,
-        fours: 0,
-        fives: 0,
-        sixes: 0
-    }
+    possibleScores: {}
 }
 
 export default function (state = initialState, action) {
     switch (action.type) {
         case CALC_POSSIBLE_SCORES:
-            console.log(action.payload);
-            return state;
+            return {
+                ...state, 
+                possibleScores: action.payload
+            };
         default: 
             return state;
     }
