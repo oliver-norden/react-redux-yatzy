ae40185b3347d818f998b44c886c085e1412056d
Oliver Nordén <oliver@tapirens.se>
2019-09-23 11:02:06 +0200

    Number of turns in rules (Dynamic)

--- src/reducers/rulesReducers.js
+++ src/reducers/rulesReducers.js
@@ -1,28 +1,33 @@
+const playingField = [
+    {header: 'Ones', score: '1', user: true},
+    {header: 'Twos', score: '2', user: true},
+    {header: 'Threes', score: '3', user: true},
+    {header: 'Fours', score: '4', user: true},
+    {header: 'Fives', score: '5', user: true},
+    {header: 'Sixes', score: '6', user: true},
+    {header: 'Sum', type: 'singlesSum'},
+    {header: 'Bonus', score: 'bonus'},
+    {header: '1 pair', score: 'one pair', user: true},
+    {header: '2 pairs', score: 'two pairs', user: true},
+    {header: 'Three of a kind', score: 'threeOfAKind', user: true},
+    {header: 'Four of a kind', score: 'fourOfAKind', user: true},
+    {header: 'Small straight', score: 'small straight', user: true},
+    {header: 'Large straight', score: 'large straight', user: true},
+    {header: 'Full house', score: 'full house', user: true},
+    {header: 'Chance', score: 'chance', user: true},
+    {header: 'Yatzy', score: 'yatzy', user: true},
+    {header: 'Sum', type: 'scoreSum'}
+];
+
+const noOfTurns = playingField.reduce((noOfTurns, row) => row.user ? noOfTurns + 1 : noOfTurns, 0) // Calculate total number of turns (add one for each row where user input is required)
+
 const initialState = {
     dice: 5,
     diceSides: 6,
     bonus: 50,
     bonusRequirment: 63,
-    playingField: [
-        {header: 'Ones', score: '1'},
-        {header: 'Twos', score: '2'},
-        {header: 'Threes', score: '3'},
-        {header: 'Fours', score: '4'},
-        {header: 'Fives', score: '5'},
-        {header: 'Sixes', score: '6'},
-        {header: 'Sum', type: 'singlesSum'},
-        {header: 'Bonus', score: 'bonus'},
-        {header: '1 pair', score: 'one pair'},
-        {header: '2 pairs', score: 'two pairs'},
-        {header: 'Three of a kind', score: 'threeOfAKind'},
-        {header: 'Four of a kind', score: 'fourOfAKind'},
-        {header: 'Small straight', score: 'small straight'},
-        {header: 'Large straight', score: 'large straight'},
-        {header: 'Full house', score: 'full house'},
-        {header: 'Chance', score: 'chance'},
-        {header: 'Yatzy', score: 'yatzy'},
-        {header: 'Sum', type: 'scoreSum'}
-    ],
+    turns: noOfTurns,
+    playingField: playingField,
     combinations: [
         {
             name: 'one pair',
